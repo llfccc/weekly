b95ca68cd34c7e14ba14a967e566e458ce0f1ebe
llfccc <llfccc@live.cn>
2017-05-09 09:20:34 +0800

    添加销售周报模型

--- weekly/api/models.py
+++ weekly/api/models.py
@@ -1,59 +1,151 @@
 # coding=utf-8
 from __future__ import unicode_literals
-
 from django.db import models
-from django.contrib.auth.models import User 
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class DevProject(models.Model):
-    project_name= models.CharField(max_length=100,verbose_name='项目名称')    
-    creator_id=models.IntegerField(verbose_name='创建人')
-    status= models.CharField(max_length=64,verbose_name='项目状态：00，关闭；01，启动')
-    remark= models.CharField(max_length=64,null=True,blank=True,verbose_name='备注')
-    create_time= models.DateTimeField(auto_now_add=True) 
+    project_name = models.CharField(max_length=100, verbose_name='项目名称')
+    
+    status = models.CharField(max_length=64, verbose_name='项目状态：00，关闭；01，启动')
+    remark = models.CharField(max_length=64, null=True,
+                              blank=True, verbose_name='备注')
+    create_time = models.DateTimeField(auto_now_add=True)
 
+    creator =  models.ForeignKey(User, verbose_name='创建人') 
     def __unicode__(self):
         return u"{}".format(self.project_name)
 
-        
-class DevEventType(models.Model):
-    event_type_name= models.CharField(max_length=100,verbose_name='事件类型名称')   
-    remark= models.CharField(max_length=64,null=True,blank=True,verbose_name='备注')
-    creator_id=models.IntegerField(verbose_name='创建人')
-    create_time= models.DateTimeField(auto_now_add=True) 
 
+class DevEventType(models.Model):
+    event_type_name = models.CharField(max_length=100, verbose_name='事件类型名称')
+    remark = models.CharField(max_length=64, null=True,
+                              blank=True, verbose_name='备注')
+    create_time = models.DateTimeField(auto_now_add=True)
+
+    # creator =  models.ForeignKey(User, verbose_name='创建人') 
     def __unicode__(self):
-        return u"{}".format(self.event_name)
+        return u"{}".format(self.event_type_name)
+
 
 class DevEvent(models.Model):
-    description =models.CharField(max_length=500,verbose_name='事件描述')
-    start_time = models.DateTimeField( verbose_name='开始时间')
+    description = models.CharField(
+        null=True, blank=True, max_length=500, verbose_name='事件描述')
+    start_time = models.DateTimeField(verbose_name='开始时间')
     end_time = models.DateTimeField(verbose_name='结束时间')
-    fin_percentage = models.IntegerField(default=0, verbose_name='完成百分比') 
-    up_reporter_id=models.CharField(max_length=64,verbose_name='上游汇报人')
-    down_reporter_ids=models.CharField(max_length=64,verbose_name='下游汇报人')
+    fin_percentage = models.IntegerField(default=0, verbose_name='完成百分比')
+    up_reporter_id = models.CharField(max_length=64, verbose_name='上游汇报人')
+    down_reporter_ids = models.CharField(max_length=64, verbose_name='下游汇报人')
     remark = models.TextField(null=True, blank=True, verbose_name="备注")
-    create_time= models.DateTimeField(auto_now_add=True)
+    create_time = models.DateTimeField(auto_now_add=True)
 
-    owner= models.ForeignKey(User, verbose_name='事件所属人')    
-    project= models.ForeignKey(DevProject, verbose_name='所属项目')    
+    owner = models.ForeignKey(User, verbose_name='事件所属人')
+    project = models.ForeignKey(DevProject, verbose_name='所属项目')
     event_type = models.ForeignKey(DevEventType, verbose_name='事件类型')
-
-
 
     def __unicode__(self):
         return u"{}".format(self.description)
 
 
-
 class WeekSummary(models.Model):
-
-    start_time = models.DateTimeField( verbose_name='开始时间')
+    start_time = models.DateTimeField(verbose_name='开始时间')
     end_time = models.DateTimeField(verbose_name='结束时间')
-    summary=models.CharField(max_length=500,verbose_name='总结')
-    self_evaluation=models.CharField(max_length=500,verbose_name='总结')
-    plan=models.CharField(max_length=500,verbose_name='总结')
-    create_time= models.DateTimeField(auto_now_add=True)
+    summary = models.CharField(max_length=500, verbose_name='总结')
+    self_evaluation = models.CharField(max_length=500, verbose_name='自我评价')
+    plan = models.CharField(max_length=500, verbose_name='计划')
+    create_time = models.DateTimeField(auto_now_add=True)
 
-    owner= models.ForeignKey(User, verbose_name='事件所属人')    
+    owner = models.ForeignKey(User, verbose_name='事件所属人')
+
+    def __unicode__(self):
+        return u"{}".format(self.summary)
+
+
+class SaleActiveType(models.Model):
+    active_type_name = models.CharField(max_length=100, verbose_name='活动类型名称')
+    remark = models.CharField(max_length=64, null=True,
+                              blank=True, verbose_name='备注')
+    creator = models.ForeignKey(User, verbose_name='事件所属人')
+    create_time = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return u"{}".format(self.active_type_name)
+
+
+class SaleCustomer(models.Model):
+    full_name = models.CharField(max_length=100, verbose_name='客户全称')
+    short_name = models.CharField(max_length=100, verbose_name='客户简称')
+    contact_post = models.CharField(max_length=100, verbose_name='主要联系人职位')
+    contact_name = models.CharField(max_length=100, verbose_name='主要联系人姓名')
+    contact_mdn = models.CharField(max_length=100, verbose_name='主要联系人手机号码')
+    contact_tel_num = models.CharField(
+        max_length=100, verbose_name='主要联系人电话号码')
+    remark = models.CharField(max_length=64, null=True,
+                              blank=True, verbose_name='客户备注')
+    create_time = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return u"{}".format(self.full_name)
+
+
+class SalePhase(models.Model):
+    phase_name = models.CharField(
+        max_length=64, verbose_name='阶段名称，例如B,C,D,E,F,G')
+    description = models.CharField(
+        null=True, blank=True, max_length=500, verbose_name='阶段描述')
+    phase_count = models.IntegerField(verbose_name='最大拜访次数')
+    remark = models.CharField(max_length=64, null=True,
+                              blank=True, verbose_name='备注')
+    create_time = models.DateTimeField(auto_now_add=True)
+
+    # creator =models.ForeignKey(User, verbose_name='创建人')
+
+    def __unicode__(self):
+        return u"{}".format(self.phase_name)
+
+
+class SaleTarget(models.Model):
+    start_time = models.DateTimeField(verbose_name='开始时间')
+    end_time = models.DateTimeField(verbose_name='结束时间')
+    target = models.CharField(max_length=500, verbose_name='目标，json存储')
+    phase_count = models.IntegerField(verbose_name='最大拜访次数')
+    remark = models.CharField(max_length=64, null=True,
+                              blank=True, verbose_name='销售目标备注')
+    create_time = models.DateTimeField(auto_now_add=True)
+
+    # creator =  models.ForeignKey(User, verbose_name='创建人') 
+    owner = models.ForeignKey(User, verbose_name='目标所属人')
+
+    def __unicode__(self):
+        return u"{}".format(self.owner)
+
+
+class SaleEvent(models.Model):
+    cus_con_post = models.CharField(max_length=500, verbose_name='客户职位')
+    visit_date = models.DateTimeField(verbose_name='拜访时间')
+    # end_time = models.DateTimeField(verbose_name='结束时间')
+    cus_con_mdn = models.CharField(max_length=64, default=None, verbose_name='手机号码')
+    cus_con_tel_num = models.CharField(max_length=64, verbose_name='客户联系方式')
+    cus_con_wechart = models.CharField(max_length=64, verbose_name='客户的微信号')
+    communicate_record = models.CharField(max_length=500, verbose_name='沟通成果')
+    remark = models.TextField(null=True, blank=True, verbose_name="备注")
+    create_time = models.DateTimeField(auto_now_add=True)
+
+    owner = models.ForeignKey(User, verbose_name='拜访人')
+    active_type = models.ForeignKey(SaleActiveType, verbose_name='拜访类型')
+    sale_customer = models.ForeignKey(SaleCustomer, verbose_name='客户ID')
+    sale_phase = models.ForeignKey(SalePhase, verbose_name='拜访阶段')
+
+    def __unicode__(self):
+        return u"{}".format(self.visit_date)
+
+class Department(models.Model):
+    department_name = models.CharField(max_length=100, verbose_name='活动类型名称')
+    remark = models.CharField(max_length=64, null=True,
+                              blank=True, verbose_name='备注')
+    create_time = models.DateTimeField(auto_now_add=True)
+
+    creator =  models.ForeignKey(User, verbose_name='创建人') 
+    def __unicode__(self):
+        return u"{}".format(self.active_type_name)
