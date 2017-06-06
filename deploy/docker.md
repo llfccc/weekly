e7a95009b6c3910b0a59f08b94663500b62ae3ce
llfccc <llfccc@live.cn>
2017-06-06 09:31:44 +0800

    完成linux环境配置

--- deploy/docker.md
+++ deploy/docker.md
@@ -6,3 +6,15 @@ docker exec -it week bash
 
 #执行shell
 bash /home/working/weekly/deploy/init.sh
+
+#备份还原pg sql数据
+pg_dump   -U postgres test > C:\test.bak
+psql  -U postgres -d test < /home/working/test.bak
+
+#备份还原django数据
+备份莫一个APP
+python manage.py dumpdata accounts --format=json > accounts.json
+备份整个db
+python manage.py dumpdata --format=json > bak.json
+2.恢复
+python manage.py loaddata app.json
--- deploy/init.sh
+++ deploy/init.sh
@@ -19,23 +19,19 @@ yum groupinstall -y "Development tools"
 yum -y install python-pip 
 pip install --upgrade pip
 
-
-
-
 #3.安装虚拟环境
 #pip install virtualenv -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
 
 #3.安装django flask sqlalchemy numpy 
-
 pip install numpy pandas uwsgi supervisor    -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
 pip install -r /home/working/weekly/weekly/requirements.txt -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
 
-#安装redis 和postgresql
+#安装redis
 yum install -y redis
 systemctl enable redis
 systemctl start redis
 
-#安装pg
+#安装postgresql
 yum -y install postgresql postgresql-libs postgresql-server  
 #初始化
 postgresql-setup initdb
@@ -58,6 +54,9 @@ systemctl start postgresql
 #重启pg
 systemctl restart postgresql
 #配置uwsgi
+mkdir /etc/uwsgi
+\cp -rf /home/working/weekly/deploy/uwsgi/uwsgi.ini /etc/uwsgi/
+
 #uwsgi --http :8000 --chdir /home/working/weekly/weekly --module wsgi    --processes 2 --threads 2 &
 #因为虚拟环境，需把wsgi复制到manage.py同路径，并在wsgi中加入 
 # import sys
@@ -71,14 +70,14 @@ systemctl enable nginx
 
 #配置nginx
 mkdir  /etc/nginx/vhost.d
-\cp -rf /home/working/weekly/deploy/nginx.conf /etc/nginx/nginx.conf
-\cp -rf /home/working/weekly/deploy/vhost80.conf /etc/nginx/vhost.d/
+\cp -rf /home/working/weekly/deploy/nginx/nginx.conf /etc/nginx/nginx.conf
+\cp -rf /home/working/weekly/deploy/nginx/vhost.d/  /etc/nginx/
 systemctl stop nginx 
 systemctl start nginx
 
 #配置cupervisord
-\cp -rf /home/working/weekly/deploy/supervisord.d /etc/
-\cp -rf /home/working/weekly/deploy/supervisord.conf /etc/
+\cp -rf /home/working/weekly/deploy/supervisord/supervisord.d /etc/
+\cp -rf /home/working/weekly/deploy/supervisord/supervisord.conf /etc/
 supervisord -c /etc/supervisord.conf
 
 systemctl enable supervisord
