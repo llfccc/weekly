3050fd06ae0ae589f40272688a28e2464989c10b
llfccc <llfccc@live.cn>
2017-06-16 13:20:30 +0800

    给获取用户名页面增加缓存

--- weekly/accounts/urls.py
+++ weekly/accounts/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 from accounts.views import LoginHandler, RegisterHandler, LogoutHandler,GetUsername
 from django.contrib.auth.decorators import login_required, permission_required
-
+from django.views.decorators.cache import cache_page
 
 urlpatterns = [
     url(r'^login/', LoginHandler.as_view()),
     # url(r'^register/', RegisterHandler.as_view()),
     url(r'^logout/', LogoutHandler.as_view()),
-    url(r'^get_username/', permission_required('accounts.view_chinesename')(GetUsername.as_view())),
+    url(r'^get_username/', permission_required('accounts.view_chinesename')(cache_page(60 * 60)(GetUsername.as_view()))),
 ]
